0f633d34d57633d48533daece2f58f15eab638d5
Azael859 <167912391+Azael859@users.noreply.github.com>
2024-04-30 12:28:42 -0600

    Create tarae24.java
    pedimos numeros al usuario para despues sumarlos

---
+++ tarae24.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+public class tarea24{
+    public static void main (String[] args){
+        int suma=0,contador;
+        String numeros = " ";
+        
+        do{
+            
+            contador = Integer.parseInt(JOptionPane.showInputDialog("escribe numeros enteros para ser sumados, para salir escriba cero "));
+            numeros=numeros+contador + " ";
+            suma=suma+contador;
+            
+        }while(contador != 0);
+        
+        JOptionPane.showMessageDialog(null,"los numeros son "+numeros+"\nsuma"+suma);
+    }
+}
